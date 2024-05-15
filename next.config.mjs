007183d124376b05a83048c4a246f672999f2a38
hyunsuk97 <mrbonk97@gmail.com>
2024-05-16 00:11:42 +0900

    Feat 정적 배포 기능 구현

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: 'https://mrbonk97.github.io/mrbonk97/',
+};
 
 export default nextConfig;
