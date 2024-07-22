a8cf1f213726f8d58d9597e5d5a331d811faed35
mrbonk97 <mrbonk97@gmail.com>
2024-07-22 13:02:58 +0900

    refactor: 랜딩 페이지 수정

--- app/_component/hover-text.tsx
+++ app/_component/hover-text.tsx
@@ -1,4 +1,54 @@
+"use client";
+import { motion } from "framer-motion";
+import { useState } from "react";
+
 interface HoverTextProps {
   title: string;
+  delay?: number;
 }
-export const HoverText = ({ title }: HoverTextProps) => {};
+export const HoverText = ({ title, delay = 0.7 }: HoverTextProps) => {
+  const [d, setD] = useState(delay);
+
+  setTimeout(() => {
+    setD(0);
+  }, 1000);
+
+  const hoverMotion = {
+    initial: {
+      y: 112,
+      ease: "easeOut",
+      duration: 0.2,
+      type: "tween",
+    },
+    animate: {
+      y: 0,
+      ease: "easeOut",
+      transition: {
+        duration: 0.2,
+        delay: d,
+        ease: "linear",
+      },
+    },
+    hover: {
+      y: 112,
+      transition: {
+        duration: 0.4,
+        type: "tween",
+        ease: "linear",
+      },
+    },
+  };
+
+  return (
+    <motion.div
+      initial="initial"
+      animate="animate"
+      whileHover="hover"
+      className="overflow-hidden"
+    >
+      <motion.h2 variants={hoverMotion} className="py-1">
+        {title}
+      </motion.h2>
+    </motion.div>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,71 +1,17 @@
-"use client";
-import { useEffect, useRef, useState } from "react";
+import { PageTransition } from "@/components/page-transition";
 import { CircleHello } from "./_component/circle-hello";
+import { HoverText } from "./_component/hover-text";
 
 export default function Home() {
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [isTextVisible, setIsTextVisible] = useState(false);
-  const ref = useRef<HTMLDivElement>(null);
-  const textRef1 = useRef<HTMLDivElement>(null);
-  const textRef2 = useRef<HTMLDivElement>(null);
-  const textRef3 = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    ref.current!.style.width = "0";
-    setTimeout(() => {
-      setIsLoaded(true);
-      // textRef1.current!.style.transition = "500ms";
-      // textRef2.current!.style.transition = "500ms";
-      // textRef3.current!.style.transition = "500ms";
-    }, 1000);
-
-    setTimeout(() => {
-      setIsTextVisible(true);
-    }, 700);
-  }, []);
-
-  if (!isLoaded) {
-    return (
-      <div
-        className="fixed w-screen h-screen bg-primary-400 duration-1000 z-50"
-        ref={ref}
-      />
-    );
-  }
-
   return (
-    <main className="w-full h-full lg:min-h-[750px] text-primary-400 text-4xl md:text-7xl lg:text-8xl font-extrabold tracking-widest flex flex-col gap-20 items-center justify-center">
-      <div className="relative group overflow-hidden">
-        <h2
-          className={`py-1 relative duration-500 group-hover:translate-y-28 ${
-            !isTextVisible && "translate-y-28"
-          }`}
-          ref={textRef1}
-        >
-          안녕하세요
-        </h2>
-      </div>
-      <CircleHello />
-      <div className="relative group overflow-hidden">
-        <h2
-          className={`py-1 relative duration-700 group-hover:translate-y-28 ${
-            !isTextVisible && "translate-y-28"
-          }`}
-          ref={textRef2}
-        >
-          저의 포트폴리오
-        </h2>
-      </div>
-      <div className="relative group overflow-hidden">
-        <h2
-          className={`py-1 relative group-hover:translate-y-28 ${
-            !isTextVisible && "translate-y-28"
-          }`}
-          ref={textRef3}
-          style={{ transitionDuration: "750ms" }}
-        >
-          방문에 감사드립니다.
-        </h2>
-      </div>
-    </main>
+    <>
+      <PageTransition />
+      <main className="w-full h-full lg:min-h-[750px] text-primary-400 text-4xl md:text-7xl lg:text-8xl font-extrabold tracking-widest flex flex-col gap-20 items-center justify-center">
+        <HoverText title="안녕하세요" delay={0.75} />
+        <CircleHello />
+        <HoverText title="저의 포트폴리오" delay={0.8} />
+        <HoverText title="방문에 감사드립니다." delay={0.9} />
+      </main>
+    </>
   );
 }
