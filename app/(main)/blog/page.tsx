c52ac81e0e61299c280ffc6ad012c909ff360908
mrbonk97 <mrbonk97@gmail.com>
2025-07-10 02:09:17 +0900

    chore: 블로그 테스트 페이지 삭제

--- app/(main)/blog/page.tsx
+++ app/(main)/blog/page.tsx
@@ -70,16 +70,10 @@ const BlogPage = () => {
         </Suspense>
       </section>
       <section className="mt-5 grid grid-cols-1 md:grid-cols-2 gap-10">
-        <Suspense
-          fallback={
-            <div className="text-custom-1 md:col-span-2">
-              <Bird size={64} className="mt-10 mx-auto" />
-              <p className="mt-5 text-xl font-medium text-center">등록된 게시글이 없습니다...</p>
-            </div>
-          }
-        >
-          <BlogListSection />
-        </Suspense>
+        <div className="mt-40 text-custom-1 md:col-span-2">
+          <Bird size={64} className="mt-10 mx-auto" />
+          <p className="mt-5 text-xl font-medium text-center">등록된 게시글이 없습니다...</p>
+        </div>
       </section>
     </main>
   );
