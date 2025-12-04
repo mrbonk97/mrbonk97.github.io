a040f527ab069691d6b7a424f24e9b0680ddf45e
mrbonk97 <mrbonk97@gmail.com>
2025-12-04 21:50:28 +0900

    feat: prefetch 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { HeaderGsap } from "@/components/gsap/header-gsap";
 import { OverlayGsap } from "@/components/gsap/overlay-gsap";
 import { HomeMatter } from "@/components/matter/home-matter";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
   const mainRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    router.prefetch("/about");
+  }, []);
 
   return (
     <main ref={mainRef} className="h-screen">
